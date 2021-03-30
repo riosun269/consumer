f328c06172c64912e7110b59839395133a1ba04d
Rio-Dinh <rio@gobear.com>
2021-03-30 22:55:16 +0700

    Dkjhjasds

--- src/test/java/de/kreuzwerker/cdc/messagingapp/UserServiceContractTest.java
+++ src/test/java/de/kreuzwerker/cdc/messagingapp/UserServiceContractTest.java
@@ -63,8 +63,8 @@ public class UserServiceContractTest {
 
         return builder.given(
             "User 1 exists")
-            .uponReceiving("A request to /users/6")
-            .path("/users/6")
+            .uponReceiving("A request to /users/7")
+            .path("/users/7")
             .method("GET")
             .willRespondWith()
             .status(200)
@@ -89,7 +89,7 @@ public class UserServiceContractTest {
     @PactVerification(fragment = "pactUserExists")
     @Test
     public void userExists() {
-        final User user = userServiceClient.getUser("6");
+        final User user = userServiceClient.getUser("7");
 
         assertThat(user.getName()).isEqualTo(NAME);
         assertThat(user.getLastLogin()).isEqualTo(LAST_LOGIN);
